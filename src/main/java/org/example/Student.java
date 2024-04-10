7ef6834e3912f066da46d76466415fddae07f777
anna52927 <165855749+anna52927@users.noreply.github.com>
2024-04-10 00:03:45 -0400

    edited student

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,155 +1,46 @@
 package org.example;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 
 //field for what cycle they are admitted in
 
 public class Student {
-    //Right now everything int because student will have a 0-3 in this area
-    protected int SATScore;
-    protected int GPA;
-    protected int major;
-    protected int extraCurriculars;
-    protected int essay;
-    protected int firstGen;
 
     //references the College objects when its made will be a list maybe for now i'll make it a string to be able to see how it works
     protected ArrayList<College> preference;
+    //Hash Map
+    HashMap<String, Integer> studentInfo = new HashMap<>();
 
-    public Student(int SATScore, int GPA, int major, int extraCurriculars, int essay, int firstGen, ArrayList<College> collegeRank) {
-        this.SATScore = SATScore;
-        this.GPA = GPA;
-        this.major = major;
-        this.extraCurriculars = extraCurriculars;
-        this.essay = essay;
-        this.firstGen = firstGen;
-        preference = collegePreferenceGen(collegeRank);
-
-    }
-
-    //would take in the list of colleges ranked TAKES IN STRING TO TEST
-    //WILL HAVE TO CHANGE
-    //also doing all 8 colleges
-    public ArrayList<College> collegePreferenceGen(ArrayList<College> collegeRank) {
-        Random random = new Random();
-        int prob = random.nextInt(1, 100);
-        ArrayList<College> list = new ArrayList<>();
-
-        //pick first college
-        if (prob >= 1 && prob <= 40) {
-            list.add(collegeRank.get(0));
-        } else if (prob >= 41 && prob <= 57) {
-            list.add(collegeRank.get(1));
-        } else if (prob >= 58 && prob <= 70) {
-            list.add(collegeRank.get(2));
-        } else if (prob >= 71 && prob <= 80) {
-            list.add(collegeRank.get(3));
-        } else if (prob >= 81 && prob <= 85) {
-            list.add(collegeRank.get(4));
-        } else if (prob >= 86 && prob <= 90) {
-            list.add(collegeRank.get(5));
-        } else if (prob >= 91 && prob <= 95) {
-            list.add(collegeRank.get(6));
-        } else if (prob >= 96 && prob <= 100) {
-            list.add(collegeRank.get(7));
-        }
-        collegeRank.remove(list.get(0));
-
-        //second college
-        prob = random.nextInt(1, 100);
-        if (prob >= 1 && prob <= 50) {
-            list.add(collegeRank.get(0));
-        } else if (prob >= 51 && prob <= 70) {
-            list.add(collegeRank.get(1));
-        } else if (prob >= 71 && prob <= 80) {
-            list.add(collegeRank.get(2));
-        } else if (prob >= 81 && prob <= 85) {
-            list.add(collegeRank.get(3));
-        } else if (prob >= 86 && prob <= 90) {
-            list.add(collegeRank.get(4));
-        } else if (prob >= 91 && prob <= 95) {
-            list.add(collegeRank.get(5));
-        } else if (prob >= 96 && prob <= 100) {
-            list.add(collegeRank.get(6));
-        }
-        collegeRank.remove(list.get(1));
-
-        //third college
-        prob = random.nextInt(1, 100);
-        if (prob >= 1 && prob <= 50) {
-            list.add(collegeRank.get(0));
-        } else if (prob >= 51 && prob <= 70) {
-            list.add(collegeRank.get(1));
-        } else if (prob >= 71 && prob <= 85) {
-            list.add(collegeRank.get(2));
-        } else if (prob >= 86 && prob <= 90) {
-            list.add(collegeRank.get(3));
-        } else if (prob >= 91 && prob <= 95) {
-            list.add(collegeRank.get(4));
-        } else if (prob >= 96 && prob <= 100) {
-            list.add(collegeRank.get(5));
-        }
-        collegeRank.remove(list.get(2));
-
-        //fourth college
-        prob = random.nextInt(1, 100);
-        if (prob >= 1 && prob <= 50) {
-            list.add(collegeRank.get(0));
-        } else if (prob >= 51 && prob <= 75) {
-            list.add(collegeRank.get(1));
-        } else if (prob >= 76 && prob <= 90) {
-            list.add(collegeRank.get(2));
-        } else if (prob >= 91 && prob <= 95) {
-            list.add(collegeRank.get(3));
-        } else if (prob >= 96 && prob <= 100) {
-            list.add(collegeRank.get(4));
-        }
-        collegeRank.remove(list.get(3));
-
-        //fifth college
-        prob = random.nextInt(1, 100);
-        if (prob >= 1 && prob <= 50) {
-            list.add(collegeRank.get(0));
-        } else if (prob >= 51 && prob <= 80) {
-            list.add(collegeRank.get(1));
-        } else if (prob >= 81 && prob <= 90) {
-            list.add(collegeRank.get(2));
-        } else if (prob >= 91 && prob <= 100) {
-            list.add(collegeRank.get(3));
-        }
-        collegeRank.remove(list.get(4));
-
-        //sixth college
-        prob = random.nextInt(1, 100);
-        if (prob >= 1 && prob <= 50) {
-            list.add(collegeRank.get(0));
-        } else if (prob >= 51 && prob <= 75) {
-            list.add(collegeRank.get(1));
-        } else if (prob >= 76 && prob <= 100) {
-            list.add(collegeRank.get(2));
-        }
-        collegeRank.remove(list.get(5));
-
-        //seventh college
-        prob = random.nextInt(1, 100);
-        if (prob >= 1 && prob <= 50) {
-            list.add(collegeRank.get(0));
-        } else if (prob >= 51 && prob <= 100) {
-            list.add(collegeRank.get(1));
-        }
-        collegeRank.remove(list.get(6));
-
-        //eighth college
-        list.add(collegeRank.get(0));
-
-        System.out.println(list);
-        return list;
-
+    public Student(int rigor, int classRank, int GPA, int SAT, int essay, int recommendations, int interview, int extraCurriculars, int talent, int character, int firstGen, int alumniRelation, int geoRes, int stateRes, int religion, int raceEthnicStatus, int volunteerWork, int workExp, int levelInt, int cycleNumber, ArrayList<College> collegeRank) {
+        studentInfo.put("Rigor of secondary school record", rigor);
+        studentInfo.put("Class Rank", classRank);
+        studentInfo.put("Academic GPA", GPA);
+        studentInfo.put("Standardized test scores", SAT);
+        studentInfo.put("Application Essay", essay);
+        studentInfo.put("Recommendation(s)", recommendations);
+        studentInfo.put("Interview",interview);
+        studentInfo.put("Extracurricular activities",extraCurriculars);
+        studentInfo.put("Talent/Ability",talent);
+        studentInfo.put("Character/personal qualities",character);
+        studentInfo.put("First Generation",firstGen);
+        studentInfo.put("Alumni/ae relation",alumniRelation);
+        studentInfo.put("Geographical residence",geoRes);
+        studentInfo.put("State residency",stateRes);
+        studentInfo.put("Religious affiliation/commitment",religion);
+        studentInfo.put("Racial/ethnic status",raceEthnicStatus);
+        studentInfo.put("Volunteer work",volunteerWork);
+        studentInfo.put("Work experience",workExp);
+        studentInfo.put("Level of applicant's interest",levelInt);
+        studentInfo.put("Application Cycle",cycleNumber);
+        preference = collegePref2(collegeRank);
 
     }
+
     public ArrayList<College> collegePref2(ArrayList<College> collegeRank){
         ArrayList<College> list = new ArrayList<>();
         ArrayList<Integer> collegePercents = new ArrayList<>();
+        //can change these if we want
         collegePercents.add(40);
         collegePercents.add(17);
         collegePercents.add(13);
@@ -164,19 +55,13 @@ public class Student {
             int collegeIndexRemove = 0;
 
             Random random = new Random();
-            int prob = random.nextInt(1, 100);
-
+            int prob = random.nextInt(100)+1;
             for(int j = 0; j < collegePercents.size(); j++){
-                System.out.println(collegePercents.get(j));
-                System.out.println(prob);
-                System.out.println(probAdd);
-                if (j == 0 && prob >= collegePercents.get(0)){
-                    System.out.println(prob);
+                if (j == 0 && prob <= collegePercents.get(0)){
                     list.add(collegeRank.get(0));
                 }
-
                 //probAdd + 1 represents lower limit and other one is upper
-                else if (prob <= (probAdd + 1) && prob <= collegePercents.get(j)) {
+                else if (prob >= (probAdd + 1) && prob <= (collegePercents.get(j) + probAdd)) {
                     list.add(collegeRank.get(j));
                     collegeIndexRemove = j;
                 }
@@ -185,17 +70,23 @@ public class Student {
             probAdd = probAdd - collegePercents.get(collegeIndexRemove);
             collegeRank.remove(collegeIndexRemove);
             collegePercents.remove(collegeIndexRemove);
-            System.out.println(probAdd);
-            System.out.println("2");
 
+            //sometimes the percentage doesn't add up to 100 (fixes it)
+            int hundredCheck = 0;
             for(int k = 0; k < collegePercents.size(); k++){
                 float newProb = (float) collegePercents.get(k) /probAdd * 100;
                 collegePercents.set(k, (int) newProb);
+                hundredCheck = hundredCheck + collegePercents.get(k);
+            }
+            if (hundredCheck < 100 && hundredCheck != 0){
+                collegePercents.set(0, (collegePercents.get(0) + (100 - hundredCheck)));
             }
         }
         System.out.println(list);
         return list;
     }
+
+    //will this list already be ranked probably not
     public College decide(ArrayList<College> collegeAct) {
         return collegeAct.get(0);
     }
