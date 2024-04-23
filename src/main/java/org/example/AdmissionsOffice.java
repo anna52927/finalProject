5a6d3a7312861dc8ff1ad2a22fcc9a92cf3e4a41
FelixTheFriend <165835179+FelixTheFriend@users.noreply.github.com>
2024-04-23 09:33:45 -0400

    fixing things
    breaking things

--- src/main/java/org/example/AdmissionsOffice.java
+++ src/main/java/org/example/AdmissionsOffice.java
@@ -9,21 +9,30 @@ public class AdmissionsOffice {
     private College self;  //idk what to name this
     private Map<String,Object> importance;  //ranked table of importance
     private HashMap<Integer,Double> acceptanceRate;
+    private ArrayList<Student> admittedStudents;
     private HashMap<String,Integer> majorDistributions;
     private HashMap<Integer,Integer> diversityDistributions;
     private int majorCutoff; //max # of students a major can go over, sacrifices diversity
     private int diversityCutoff; //opposite of majorCutoff
-    private int EDAccepted;   //stored data from ED rounds so accurate acceptance rate can be calculated
-    private int EDApplied; //see above comment
+    private int EDApplied; //stored datum from ED rounds so accurate acceptance rate can be calculated
+    private double EDAdmitCapacity;  //percentage of total capacity to be filled by ED
 
     public AdmissionsOffice(College college, double initialAcceptanceRate){
         this.self = college; //wow, this line looks cursed
         acceptanceRate = new HashMap<>();
+        admittedStudents = new ArrayList<>();
         acceptanceRate.put(0,initialAcceptanceRate);
         importance = JSONData.JSONImport(college.name + "ImportantMetrics.json");
     }
 
-    public ArrayList<Student> considerApplicants(ArrayList<Student> applicants, String round){
+    public ArrayList<Student> considerApplicants(ArrayList<Student> applicants,String round){
+        int capacity;
+        if(round.equals("ED")){
+            capacity = (int) (self.capacity * EDAdmitCapacity);
+        } else {
+            capacity = self.capacity;
+        }
+
         for (Student applicant:
                 applicants) {
             applicant.setScore(evaluateApplicant(applicant));
@@ -42,7 +51,6 @@ public class AdmissionsOffice {
             n--;
         }
 
-        ArrayList<Student> admittedStudents = new ArrayList<>();
         //list slicing (python superiority moment)
         int i = 0;
         while(admittedStudents.size() < self.capacity && i < applicants.size()){
@@ -52,16 +60,15 @@ public class AdmissionsOffice {
             if(majorDistributions.get(major) > diversityCutoff && diversityDistributions.get(diversity) > majorCutoff) {
                 admittedStudents.add(student);
                 majorDistributions.put(major, majorDistributions.get(major) - 1);
-                diversityDistributions.put(diversity, diversityDistributions.get(major) - 1);
+                diversityDistributions.put(diversity, diversityDistributions.get(diversity) - 1);
             }
             i++;
         }
 
         if(round.equals("ED")){
-            EDAccepted = admittedStudents.size();
             EDApplied = applicants.size();
         } else {
-            acceptanceRate.put(applicants.get(0).getHashMap().get("Application Year"), (double) admittedStudents.size() / applicants.size());
+            acceptanceRate.put(applicants.get(0).getHashMap().get("Application Year"), (double) admittedStudents.size() / (applicants.size()+EDApplied));
         }
 
         return admittedStudents;
@@ -76,6 +83,11 @@ public class AdmissionsOffice {
             score += applicant.studentInfo.get(key) * collegeVal.get("value"); //need corresponding student hashMap
         }
         return score;
+    }
+
+    public void resetRound(){
+        admittedStudents.clear();
+        EDApplied = 0;
     }
 
     public HashMap<Integer,Double> getAcceptanceRate(){
